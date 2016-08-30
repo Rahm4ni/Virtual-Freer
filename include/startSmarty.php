607e1e19da4bd9118049e1ea3bfc83416f9b2ffc
ShinTod <todlich33@gmail.com>
2016-08-30 23:05:38 +0430

    Change smarty's directory

--- include/startSmarty.php
+++ include/startSmarty.php
@@ -4,6 +4,7 @@
   http://freer.ir/virtual
 
   Copyright (c) 2011 Mohammad Hossein Beyram, freer.ir
+  Unofficial version edit on 8/30/2016: Mohammad Rahmani, @3hinTod
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License v3 (http://www.gnu.org/licenses/gpl-3.0.html)
@@ -13,7 +14,7 @@
 define('SITE_ROOT', dirname(dirname(__FILE__)));
 define('PRESENTATION_DIR', SITE_ROOT . '/templates/');
 define('BUSINESS_DIR', SITE_ROOT . '/business/');
-define('SMARTY_DIR', SITE_ROOT . '/include/libs/smarty/');
+define('SMARTY_DIR', SITE_ROOT . '/include/smarty/');
 define('TEMPLATE_DIR', PRESENTATION_DIR . 'templates');
 define('COMPILE_DIR', ($config['smarty']['compile_dir'] != '') ? $config['smarty']['compile_dir'] : PRESENTATION_DIR . 'templates_c');
 define('CONFIG_DIR', SITE_ROOT . '/include/smartyConfigs');
